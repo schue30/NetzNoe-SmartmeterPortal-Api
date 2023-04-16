213155bed465ae8fdb2af190cc9b316ba90c535d
Mathias Schüpany <mathias@schuepany.at>
2023-04-16 22:45:21 +0200

    cleanup models

--- src/netznoe_smartmeter_portal_api/models.py
+++ src/netznoe_smartmeter_portal_api/models.py
@@ -6,22 +6,21 @@ from pendulum import Date, DateTime
 
 @dataclass
 class SmartmeterResult:
-    consumption_metered: Dict[Union[Date, DateTime], float]  # Verbrauch gemessen in kWh
-    consumption_estimated: Dict[Union[Date, DateTime], float]  # Verbrauch geschaetzt in kWh
+    consumption_metered: Dict[Union[Date, DateTime], float]
+    consumption_estimated: Dict[Union[Date, DateTime], float]
     grid_usage_leftover: Dict[Union[Date, DateTime], float]
     self_coverage: Dict[Union[Date, DateTime], float]
     self_coverage_renewable_energy: Dict[Union[Date, DateTime], float]
     joint_tenancy_proportion: Dict[Union[Date, DateTime], float]
-    peak_demands_metered: Dict[Union[Date, DateTime], float]  # Leistung gemessen in kW
-    peak_demands_estimated: Dict[Union[Date, DateTime], float]  # Leistung geschaetzt in kW
+    peak_demands_metered: Dict[Union[Date, DateTime], float]
+    peak_demands_estimated: Dict[Union[Date, DateTime], float]
 
 
 @dataclass
 class SmartmeterResultYearly:
-    consumption: Dict[Union[Date, DateTime], float]  # Verbrauch in kWh
+    consumption: Dict[Union[Date, DateTime], float]
     grid_usage_leftover: Dict[Union[Date, DateTime], float]
     self_coverage: Dict[Union[Date, DateTime], float]
     self_coverage_renewable_energy: Dict[Union[Date, DateTime], float]
     joint_tenancy_proportion: Dict[Union[Date, DateTime], float]
-    peak_demands: Dict[Union[Date, DateTime], float]  # Leistung in kW
-    # is_mixed -> don't know that this does (true or false)
+    peak_demands: Dict[Union[Date, DateTime], float]
